bd1a29f3b0322d9070ce13e93c8b89ff9ab04d9f
Daniele Campari <danielecampari@gmail.com>
2022-02-18 13:30:32 +0300

    Score and winning logic

--- js/main.js
+++ js/main.js
@@ -16,42 +16,6 @@ function computerPlay(){
 }
 
 /**
-* This function let the player select the play and call playRound with
-* player selection and computer random selection.
-*/
-function playerPlay(){
-
-    const buttons = document.querySelectorAll('.btn');
-    let playerWin = 0;
-    let computerWin = 0;
-
-    const playerScore = document.querySelector('#player-score');
-    const computerScore = document.querySelector('#computer-score');
-
-    buttons.forEach(button => {
-        button.addEventListener('click', () => {
-            const computer = computerPlay();
-            const winner = playRound(button.dataset.play, computer);
-            console.log(winner);
-
-            if (winner === 'player') {
-                playerWin++;
-                playerScore.textContent = playerWin;
-                console.log(`You Win the Round! Player: ${playerWin} - Computer: ${computerWin}`);
-                } else if (winner === 'computer') {
-                    computerWin++;
-                    computerScore.textContent = computerWin;
-                    console.log(`You Lose the Round! Player: ${playerWin} - Computer: ${computerWin}`);
-                }
-                // else {
-                //
-                //     console.log('It\'s a tie. Play again this round');
-                // }
-        });
-    });
-}
-
-/**
 * This function normalize the inputs from player and computer,
 * play a round of the game and return who win the round (or a tie).
 * @param {string} playerSelection The play from player.
@@ -70,7 +34,7 @@ function playRound(player, computer){
     }
 
     if (player > computer) {
-        if (player === 'rock' || computer === 'rock') {
+        if (player === 'Rock' || computer === 'Rock') {
             result.textContent = `${player} - ${computer}. Computer won the round`;
             return 'computer';
         } else {
@@ -78,7 +42,7 @@ function playRound(player, computer){
             return 'player';
         }
     } else {
-        if (player === 'rock' || computer === 'rock') {
+        if (player === 'Rock' || computer === 'Rock') {
             result.textContent = `${player} - ${computer}. Player won the round`;
             return 'player';
         } else {
@@ -89,20 +53,80 @@ function playRound(player, computer){
 }
 
 /**
+* This function let the player select the play and call playRound with
+* player selection and computer random selection.
+*/
+
+function game(){
+
+    const buttons = document.querySelectorAll('.btn');
+    let playerWin = 0;
+    let computerWin = 0;
+
+    const playerScore = document.querySelector('#player-score');
+    const computerScore = document.querySelector('#computer-score');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            const computer = computerPlay();
+            const winner = playRound(button.dataset.play, computer);
+
+            if (winner === 'player') {
+                playerWin++;
+                playerScore.textContent = playerWin;
+            }
+
+            if (winner === 'computer') {
+                computerWin++;
+                computerScore.textContent = computerWin;
+            }
+
+            if (playerWin === 5 || computerWin === 5){
+                finalScore(playerWin, computerWin);
+                buttons.forEach(button => {
+                    button.setAttribute('disabled', true);
+                })
+            }
+        });
+    });
+}
+
+function finalScore(plyScore, cpuScore) {
+    const final = document.querySelector('#final');
+    const reload = document.querySelector('#reload');
+
+
+    if (plyScore > cpuScore) {
+        final.textContent = 'Player win the game';
+    } else {
+        final.textContent = 'Computer win the game';
+    }
+
+    const btnReload = document.createElement('button');
+    btnReload.textContent = 'Play Again';
+    btnReload.setAttribute('id', 'reload');
+
+    reload.appendChild(btnReload);
+
+    btnReload.addEventListener('click', () => {
+        window.location.reload();
+    });
+}
+/**
 * This function play a match of five rounds, compute the winning of each round
 * and display it. If there is a tie the round is repeated.
 * After five round compute and displey the winner of the match.
 */
-function game() {
-    let playerWin = 0;
-    let computerWin = 0;
-
-    for(let i = 0; i <5; i++) {
-        const winner = playerPlay();
-        playerWin++;
-        console.log(winner);
-
-    }
+// function game() {
+//     let playerWin = 0;
+//     let computerWin = 0;
+//
+//     for(let i = 0; i <5; i++) {
+//         const winner = playerPlay();
+//         playerWin++;
+//         console.log(winner);
+//
+//     }
 
             // for (let i = 0; i < 5; i++) {
     //
@@ -126,6 +150,6 @@ function game() {
     // } else {
     //     console.log(`You Lose the Match! Final score: ${playerWin} for Player - ${computerWin} for Computer`);
     // }
-}
+//}
 
-playerPlay();
+game();
